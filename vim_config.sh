f2236a6c26d51852886d551a3289ac28332387f9
tormozzg <dark.mozzg@gmail.com>
2020-01-27 19:38:14 +0300

    Update vim_config.sh

--- vim_config.sh
+++ vim_config.sh
@@ -2,16 +2,18 @@
 
 mkdir -p ~/.vim/colors
 if [ -x "$(command -v wget)" ]; then
-    wget -q https://raw.githubusercontent.com/tormozzg/tormozzg.github.io/master/lucius.vim -O ~/.vim/colors/lucius.vim
+    wget -q https://raw.githubusercontent.com/tormozzg/configs/master/lucius.vim -O ~/.vim/colors/lucius.vim
     echo 'Wget: Colors downloaded'
 elif [ -x "$(command -o curl)" ]; then
-    curl -s https://raw.githubusercontent.com/tormozzg/tormozzg.github.io/master/lucius.vim --output ~/.vim/colors/lucius.vim
+    curl -s https://raw.githubusercontent.com/tormozzg/configs/master/lucius.vim --output ~/.vim/colors/lucius.vim
     echo 'Curl: Colors downloaded'
 else
     echo 'Error: Please install wget or curl;'
     exit 1
 fi
 
+echo "filetype plugin indent on" >> ~/.vimrc
+echo "set expandtab" >> ~/.vimrc
 echo "set tabstop=2" >> ~/.vimrc
 echo "set shiftwidth=2" >> ~/.vimrc
 echo "set smarttab" >> ~/.vimrc
@@ -26,8 +28,4 @@ echo "set number" >> ~/.vimrc
 echo "syntax on" >> ~/.vimrc
 echo "colorscheme lucius" >> ~/.vimrc
 echo "set background=light" >> ~/.vimrc
-echo "set expandtab" >> ~/.vimrc
-echo "set tabstop=2" >> ~/.vimrc
-echo "set shiftwidth=2" >> ~/.vimrc
-echo "set tabstop=4 softtabstop=0 expandtab shiftwidth=2 smarttab" >> ~/.vimrc
 echo "set softtabstop=0 noexpandtab" >> ~/.vimrc
