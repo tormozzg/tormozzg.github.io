90fcee06c22172738827ca5407758bc826ac419f
TorMozzG <tormozzg@gmail.com>
2020-11-22 18:26:41 +0300

    Vim configuration has been changed: Lucius theme has been removed.

--- vim_config.sh
+++ vim_config.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-mkdir -p ~/.vim/colors
-if [ -x "$(command -v wget)" ]; then
-    wget -q https://raw.githubusercontent.com/tormozzg/tormozzg.github.io/master/lucius.vim -O ~/.vim/colors/lucius.vim
-    echo 'Wget: Colours has been downloaded'
-elif [ -x "$(command -o curl)" ]; then
-    curl -s https://raw.githubusercontent.com/tormozzg/tormozzg.github.io/master/lucius.vim --output ~/.vim/colors/lucius.vim
-    echo 'Curl: Colours has been downloaded'
-else
-    echo 'Error: Please install wget or curl;'
-    exit 1
-fi
 
 echo "filetype plugin indent on" >> ~/.vimrc
 echo "set expandtab" >> ~/.vimrc
@@ -26,6 +15,5 @@ echo "set incsearch" >> ~/.vimrc
 echo "set ignorecase" >> ~/.vimrc
 echo "set number" >> ~/.vimrc
 echo "syntax on" >> ~/.vimrc
-echo "colorscheme lucius" >> ~/.vimrc
 echo "set background=light" >> ~/.vimrc
 echo "set softtabstop=0 noexpandtab" >> ~/.vimrc
