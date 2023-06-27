b1176428316b1e3c39c10e0b70b6d617c68484f7
tormozzg <tormozzg@gmail.com>
2023-06-27 21:46:09 +0300

    Revert "Vim configuration has been changed: Lucius theme has been removed."
    This reverts commit 90fcee06c22172738827ca5407758bc826ac419f.

--- vim_config.sh
+++ vim_config.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+mkdir -p ~/.vim/colors
+if [ -x "$(command -v wget)" ]; then
+    wget -q https://raw.githubusercontent.com/tormozzg/tormozzg.github.io/master/lucius.vim -O ~/.vim/colors/lucius.vim
+    echo 'Wget: Colours has been downloaded'
+elif [ -x "$(command -o curl)" ]; then
+    curl -s https://raw.githubusercontent.com/tormozzg/tormozzg.github.io/master/lucius.vim --output ~/.vim/colors/lucius.vim
+    echo 'Curl: Colours has been downloaded'
+else
+    echo 'Error: Please install wget or curl;'
+    exit 1
+fi
 
 echo "filetype plugin indent on" >> ~/.vimrc
 echo "set expandtab" >> ~/.vimrc
@@ -14,5 +25,6 @@ echo "set hlsearch" >> ~/.vimrc
 echo "set incsearch" >> ~/.vimrc
 echo "set ignorecase" >> ~/.vimrc
 echo "syntax on" >> ~/.vimrc
+echo "colorscheme lucius" >> ~/.vimrc
 echo "set background=light" >> ~/.vimrc
 echo "set softtabstop=0 noexpandtab" >> ~/.vimrc
